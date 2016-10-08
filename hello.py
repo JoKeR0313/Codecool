ac4e9146fcf460530ba4860a26fb02fdcb87b701
JoKeR0313 <istvanfilimon@gmail.com>
2016-10-08 14:37:06 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,10 +2,10 @@ import sys
 
 def hellopy():	#Definition of hellopy function
 
-		if len(sys.argv) > 1:			#If you give a name, it will welcomes it
+		if len(sys.argv) > 1:	#If you give a name, it will welcomes it
 			print("Hello " + sys.argv[1] + "!")
 		
-		else:											#If you didn't, it will welcomes the whole world
+		else:	#If you didn't, it will welcomes the whole world
 			print("Hello World!")
 
-hellopy()				#Running function	
+hellopy() #Running function	
