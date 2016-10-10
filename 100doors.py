3093bca7a9ce6444689f221032e927426eddaaeb
JoKeR0313 <istvanfilimon@gmail.com>
2016-10-10 22:11:15 +0200

    Add files via upload

---
+++ 100doors.py
@@ -0,0 +1,15 @@
+print("The following doors are open:")
+
+doors = [0] * 100  #Create a list, with 100 pcs of 0.
+
+for x in range (1, 101):
+    for status in range (100):
+        if (status + 1) % x == 0:  #status+1 (0 is the first index)
+            if doors[status] == 0: #0 means that the door is closed, 1 means the opened status.
+               doors[status] = 1   
+            else:
+                doors[status] = 0
+
+for status in range(100): #Counting the open doors.
+    if doors[status] == 1:
+        print(status+1 ,end=" ")
